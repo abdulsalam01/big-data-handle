523ee1fb049cd72264778cb9325c6f10f3500249
Agung Budi Prasetyo <agungbudi3@gmail.com>
2021-03-11 10:44:48 +0700

    update request & response

--- app/Http/Controllers/BacameterController.php
+++ app/Http/Controllers/BacameterController.php
@@ -11,30 +11,34 @@ class BacameterController extends Controller
     //
     public function getDetail($id, Request $request) {
         $take = $request->query('take') ?? 10;
-        $skip = $request->query('skip') ?? 0;
+        $current_page = $request->query('page') ?? 1;
+        $skip = ($current_page - 1)*$take;
+
         $rayon = Rayon::where('rayon_id', $id)->first();
         $total =
-        DB::table("ms_pelanggan as a")
-            ->selectRaw("COALESCE(COUNT(*), 0) as cnt")
-            ->join("ms_jenispelanggan_tarif as b", "a.jenis_pelanggan_id", "b.jenispelanggan_id")
-            ->where([
-                ["cabang_id", "=", $rayon->cabang_id],
-                ["wilayah_id", "=", $rayon->wilayah_id],
-                ["blok_id", "=", $rayon->blok_id],
-                ["status_id", 1]
-            ])
-            ->get()
-            ->pluck("cnt")[0];
+            DB::table("ms_pelanggan as a")
+                ->selectRaw("COALESCE(COUNT(*), 0) as cnt")
+                ->join("ms_jenispelanggan_tarif as b", "a.jenis_pelanggan_id", "b.jenispelanggan_id")
+                ->where([
+                    ["cabang_id", "=", $rayon->cabang_id],
+                    ["wilayah_id", "=", $rayon->wilayah_id],
+                    ["blok_id", "=", $rayon->blok_id],
+                    ["status_id", 1]
+                ])
+                ->get()
+                ->pluck("cnt")[0];
+
+        $last_page = ceil($total/$take);
 
         $query =
-        DB::query()
-            ->select(DB::raw("a.*, b.jenispelanggan_name as sub_jenis_pelanggan,
+            DB::query()
+                ->select(DB::raw("a.*, b.jenispelanggan_name as sub_jenis_pelanggan,
                 b.tarif1, b.tarif2, b.tarif3, b.biaya_pemeliharaan,
                 b.biaya_administrasi, b.biaya_denda, p1.angkakini_1bulanlalu,
                 p2.angkalalu_1bulanlalu, p2.angkalalu_1bulanlalu as angkakini_2bulanlalu,
                 p3.angkalalu_2bulanlalu, p3.angkalalu_2bulanlalu as angkakini_3bulanlalu,
                 p4.angkalalu_3bulanlalu"))
-            ->from(DB::raw("ms_pelanggan a, ms_jenispelanggan_tarif b, (
+                ->from(DB::raw("ms_pelanggan a, ms_jenispelanggan_tarif b, (
                 SELECT p.pelanggan_id, SUM(P.stand_angka) AS angkakini_1bulanlalu
                 FROM tbl_bacameter P
                 WHERE to_char(P.waktu_baca, 'YYYY-MM' :: TEXT) =
@@ -62,32 +66,41 @@ class BacameterController extends Controller
                     to_char (( now() - '4 mons' :: INTERVAL ), 'YYYY-MM' :: TEXT )
                 GROUP BY p.pelanggan_id
                 ORDER BY p.pelanggan_id asc) p4"))
-            ->where([
-                ["a.id", "=", DB::raw("p1.pelanggan_id::INTEGER")],
-                ["a.id", "=", DB::raw("p2.pelanggan_id::INTEGER")],
-                ["a.id", '=', DB::raw("p3.pelanggan_id::INTEGER")],
-                ["a.id", '=', DB::raw("p4.pelanggan_id::INTEGER")],
-                ["a.jenis_pelanggan_id", "=", DB::raw("b.jenispelanggan_id::INTEGER")],
-                ["cabang_id", "=", $rayon->cabang_id],
-                ["wilayah_id", "=", $rayon->wilayah_id],
-                ["blok_id", "=", $rayon->blok_id],
-                ["status_id", 1]
-            ])
-            ->orderBy('a.id')
-            ->take($take)
-            ->skip($skip)
-            ->get();
-            // ->chunk(50, function($data) use(&$query) {
-            //     foreach ($data as $key => $value) {
-            //         array_push($query, $value);
-            //     }
-            // });
+                ->where([
+                    ["a.id", "=", DB::raw("p1.pelanggan_id::INTEGER")],
+                    ["a.id", "=", DB::raw("p2.pelanggan_id::INTEGER")],
+                    ["a.id", '=', DB::raw("p3.pelanggan_id::INTEGER")],
+                    ["a.id", '=', DB::raw("p4.pelanggan_id::INTEGER")],
+                    ["a.jenis_pelanggan_id", "=", DB::raw("b.jenispelanggan_id::INTEGER")],
+                    ["cabang_id", "=", $rayon->cabang_id],
+                    ["wilayah_id", "=", $rayon->wilayah_id],
+                    ["blok_id", "=", $rayon->blok_id],
+                    ["status_id", 1]
+                ])
+                ->orderBy('a.id')
+                ->take($take)
+                ->skip($skip)
+                ->get();
+        // ->chunk(50, function($data) use(&$query) {
+        //     foreach ($data as $key => $value) {
+        //         array_push($query, $value);
+        //     }
+        // });
 
-        return response()->json([
+        $data = [
             'data' => $query,
+            'current_page'=>(int)$current_page,
+            'last_page'=>$last_page,
             'take' => $take,
             'skip' => $skip,
             'total' => $total
-        ], 200);
+        ];
+        return response()->json(
+            [
+                'code' => 200,
+                'status' => true,
+                'data' =>$data
+            ]
+            , 200);
     }
 }
